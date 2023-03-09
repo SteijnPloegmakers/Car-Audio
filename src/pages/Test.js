340ed715bf2063b3c947472ed508d663e989b241
Steijn Ploegmakers <461991@student.fontys.nl>
2023-03-09 15:08:09 +0100

    changed cache so offline html shows up when offline

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -11,8 +11,8 @@ function Test() {
   const authEndpoint = "https://accounts.spotify.com/authorize/?"
 
   const clientId = "c12a19b4c59744a797e50a4c058b753e"
-  //const redirectUri = "https://i491216.hera.fhict.nl"
-  const redirectUri = "https://localhost:3000"
+  const redirectUri = "https://i491216.hera.fhict.nl"
+  //const redirectUri = "https://localhost:3000"
   const scopes = [
     'user-read-currently-playing',
     'user-read-playback-state',
